0c6c4d97ffc6219d1748fa550671441257801ebe
Loyda Lopez <loydalopez1@gmail.com>
2019-09-30 00:30:39 +0000

    adding maps

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,64 +1,66 @@
 class PlacesController < ApplicationController
+  
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
+     
    @places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
   end
 
-  def new
-    @place = Place.new
-  end
+   def new
+     @place = Place.new
+   end
 
-  def create
- @place = current_user.places.create(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+   def create
+  @place = current_user.places.create(place_params)
+     if @place.valid?
+       redirect_to root_path
+     else
+       render :new, status: :unprocessable_entity
+     end
+   end
 
-  def show
-    @place = Place.find(params[:id])
-  end
+   def show
+     @place = Place.find(params[:id])
+   end
 
-  def edit
-    @place = Place.find(params[:id])
+   def edit
+     @place = Place.find(params[:id])
 
     if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-  end
+       return render plain: 'Not Allowed', status: :forbidden
+     end
+   end
 
-  def update
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+   def update
+     @place = Place.find(params[:id])
+     if @place.user != current_user
+       return render plain: 'Not Allowed', status: :forbidden
+     end
 
-    @place.update_attributes(place_params)
-    if @place.valid?
-        redirect_to root_path
-    else
-      render :edit, status: :unprocessable_entity
+     @place.update_attributes(place_params)
+     if @place.valid?
+         redirect_to root_path
+     else
+       render :edit, status: :unprocessable_entity
     end
-  end
+   end
 
-  def destroy
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+   def destroy
+     @place = Place.find(params[:id])
+     if @place.user != current_user
+       return render plain: 'Not Allowed', status: :forbidden
+     end
 
     @place.destroy
-    redirect_to root_path
-  end
+     redirect_to root_path
+   end
 
-  private
+   private
 
-  def place_params
+   def place_params
     params.require(:place).permit(:name, :description, :address)
-  end
+   end
 end
 
   
