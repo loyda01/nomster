7ab9818a61d85ef6268e0c76b886b9ee739612d5
Loyda Lopez <loydalopez1@gmail.com>
2019-10-02 23:44:21 +0000

    sign in issues

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,69 +1,68 @@
 class PlacesController < ApplicationController
-  
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  
+
   def index
-     
-   @places = Place.order(:id).paginate(:page => params[:page], :per_page => 10)
+    @places = Place.all
   end
 
-   def new
-     @place = Place.new
-   end
+  def new
+    @place = Place.new
+  end
 
-    def create
+  def create
     @place = current_user.places.create(place_params)
-     if @place.valid?
-       redirect_to root_path
-     else
-       render :new, status: :unprocessable_entity
-     end
-   end
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-   def show
-     @place = Place.find(params[:id])
-     @comment = Comment.new
-     @photo = Photo.new
+  def show
+    @place = Place.find(params[:id])
+    @comment = Comment.new
+    @photo = Photo.new
+  end
 
-   end
-
-   def edit
-     @place = Place.find(params[:id])
+  def edit
+    @place = Place.find(params[:id])
 
     if @place.user != current_user
-       return render plain: 'Not Allowed', status: :forbidden
-     end
-   end
-
-   def update
-     @place = Place.find(params[:id])
-     if @place.user != current_user
-       return render plain: 'Not Allowed', status: :forbidden
-     end
-
-     @place.update_attributes(place_params)
-     if @place.valid?
-         redirect_to root_path
-     else
-       render :edit, status: :unprocessable_entity
+      return render plain: 'Not Allowed', status: :forbidden
     end
-   end
+  end
 
-   def destroy
-     @place = Place.find(params[:id])
-     if @place.user != current_user
-       return render plain: 'Not Allowed', status: :forbidden
-     end
+  def update
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
 
-    @place.destroy
-     redirect_to root_path
-   end
-
-   private
-
-   def place_params
-    params.require(:place).permit(:name, :description, :address)
-   end
+  @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
+
+  def destroy
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end 
+
+
+  @place.destroy
+  redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+end
+  
 
   
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
  #    password: ENV['GMAIL_PASSWORD']
  #  }
 
-   config.action_mailer.default_url_options = { host: 'localhost:3030' }
+   config.action_mailer.default_url_options = { host: 'nomster-loyda-lopez.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
